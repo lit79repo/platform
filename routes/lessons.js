ac78d24b47ea0aa99ec1a0f8ff94b53fa79e4423
Misha Marinenko <0x77dev@protonmail.com>
2019-02-20 00:56:27 +0200

    Design improvments.

--- routes/lessons.js
+++ routes/lessons.js
@@ -4,5 +4,9 @@ module.exports = (db) => {
     router.get('/', function (req, res, next) {
         res.render('lessons', { title: 'Розклад', classes: db.get("classes").value() });
     });
+    router.get('/api', function(req, res, next){
+        res.json(db.get("classes").value())
+        next();
+    });
     return router;
 };
