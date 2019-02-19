97925b1739b307d23b9f00d6933cc55f2fc1f91d
Misha Marinenko <0x77dev@protonmail.com>
2019-02-20 01:04:27 +0200

    mojs click animation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,3 +29,41 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
         console.warn('Push messaging is not supported');
         pushButton.textContent = 'Push Not Supported';
 }
+
+//Animation
+var _extends = Object.assign || function (target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i];for (var key in source) {if (Object.prototype.hasOwnProperty.call(source, key)) {target[key] = source[key];}}}return target;};
+var OPTS = {
+        fill: 'none',
+        radius: 25,
+        strokeWidth: { 50: 0 },
+        scale: { 0: 1 },
+        angle: { 'rand(-35, -70)': 0 },
+        duration: 500,
+        left: 0, top: 0,
+        easing: 'cubic.out' };
+      
+      
+      var circle1 = new mojs.Shape(_extends({},
+      OPTS, {
+        stroke: 'cyan' }));
+      
+      
+      var circle2 = new mojs.Shape(_extends({},
+      OPTS, {
+        radius: { 0: 15 },
+        strokeWidth: { 30: 0 },
+        stroke: 'magenta',
+        delay: 'rand(75, 150)' }));
+      
+      
+      document.addEventListener('click', function (e) {
+      
+        circle1.
+        tune({ x: e.pageX, y: e.pageY }).
+        replay();
+      
+        circle2.
+        tune({ x: e.pageX, y: e.pageY }).
+        replay();
+      
+      });
