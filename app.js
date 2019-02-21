18cd56be6aa1efcb514550a1b95e3857c4ef44a4
Misha Marinenko <0x77dev@protonmail.com>
2019-02-21 18:17:18 +0200

    Service Worker is working #29, Express static to webpack dist folder #31

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let db = require("./db.lib");
 let indexRouter = require('./routes/index');
 let lessonsRouter = require('./routes/lessons');
 let fmRouter = require('./routes/fm');
-
+let staticRouter = require('./routes/static');
 let app = express();
 
 let http = require("http");
@@ -16,13 +16,13 @@ setInterval(function() {
 }, 15000);
 
 app.use((req,res,next)=>{
-  res.set('Service-Worker-Allowed', 1);
+  res.set('Service-Worker-Allowed', '/');
   next();
 });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use('/', staticRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,10 +30,10 @@ app.use(cookieParser());
 app.use('/platform/api/', indexRouter);
 app.use('/platform/api/lessons', lessonsRouter(db));
 app.use('/platform/fm', fmRouter);
+
 app.use((req, res, next) => {
   next(createError(404));
 });
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
