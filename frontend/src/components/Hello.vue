e402f375b8e2dd50b1950c78c696e8c99fae1c14
Misha Marinenko <0x77dev@protonmail.com>
2019-02-23 21:38:16 +0200

    Empty State added.

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="hello">
+    <md-empty-state
+      md-icon="device_hub"
+      md-label="Виберіть пункт меню"
+      md-description="Натисніть на кнопку меню.">
+    </md-empty-state>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js PWA'
-    }
-  }
+  name: 'hello'
 }
 </script>
 
